a11508184cd61879bc111967b84f25af7b25e282
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-01-29 08:07:40 +0200

    UpdateProfileAutoMapper

---
+++ CoursatyApp/Profiles/CoursesProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CoursatyApp.DTOs;
+using CoursatyApp.Entities;
+
+namespace CoursatyApp.Profiles
+{
+    public class CoursesProfile : Profile
+    {
+        public CoursesProfile()
+        {
+            CreateMap<Course, CourseDto>()
+                .ForMember(dst => dst.Id, src => src.MapFrom(c => c.Id))
+                .ForMember(dst => dst.Category, src => src.MapFrom(c => c.CategoryId))
+                .ForMember(dst => dst.Creation, src => src.MapFrom(c => c.Creation_Date.ToShortDateString()));
+
+
+        }
+    }
+}
