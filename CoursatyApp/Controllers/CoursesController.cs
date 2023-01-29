e5bdb895099863516e91cb00235d3c8c222c18c9
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-01-29 05:14:43 +0200

    UpdateGetEndPoit

--- CoursatyApp/Controllers/CoursesController.cs
+++ CoursatyApp/Controllers/CoursesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CoursatyApp.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoursatyApp.Controllers
 {
@@ -7,5 +9,18 @@ namespace CoursatyApp.Controllers
     [ApiController]
     public class CoursesController : ControllerBase
     {
+        private readonly CoursesDbContext coursesDbContext;
+
+        public CoursesController(CoursesDbContext coursesDbContext)
+        {
+            this.coursesDbContext = coursesDbContext;
+        }
+        [HttpGet]
+        public ActionResult GetCourse()
+        {
+            var course = coursesDbContext.Courses.ToList();
+
+            return Ok(course);
+        }
     }
 }
---
+++ CoursatyApp/Migrations/20230129031046_init.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoursatyApp.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Creation_Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}
