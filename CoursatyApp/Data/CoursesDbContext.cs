a8ad7b54f54f23bbc1c378493f53c0466e00a3cc
Mo'men Ashraf Muhamed <moamen.ashraf1892001@gmail.com>
2023-01-29 04:39:44 +0200

    UpdateContext

---
+++ CoursatyApp/Data/CoursesDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoursatyApp.Data
+{
+    public class CoursesDbContext  : DbContext
+    {
+        public CoursesDbContext(DbContextOptions<CoursesDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
